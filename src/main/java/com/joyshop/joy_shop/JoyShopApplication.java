5d1628f88292a1cbf89c5fa8f0520be88516c3a1
sbwoo <woosb0115@nate.com>
2023-03-18 18:31:18 +0900

    Hello world

---
+++ src/main/java/com/joyshop/joy_shop/JoyShopApplication.java
@@ -0,0 +1,13 @@
+package com.joyshop.joy_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoyShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joyshop/joy_shop/JoyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyshop.joy_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoyShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
